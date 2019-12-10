c525404f0e8961d856c1b86d4b8832fefec58943
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2019-12-10 10:12:45 -0300

    Tutorial...

--- hello-world-structured.js
+++ hello-world-structured.js
@@ -1,22 +1,42 @@
+//SCENE
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color('#3d3d3d');
 
-var renderer = new THREE.WebGLRenderer();
+//CAMERA
+const fov = 35;
+const aspect = window.innerWidth/window.innerHeight;
+const near = 0.1;
+const far = 100;
+var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.set(0,0,10);
+camera.position.z = 5;
+
+//RENDERER
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+//CUBE
+var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+//var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+var material = new THREE.MeshStandardMaterial({color: 0xff0000});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+// Create a directional light
+const light = new THREE.DirectionalLight( 0xffffff, 5.0 );
+// move the light back and up a bit
+light.position.set( 10, 10, 10 );
+// remember to add the light to the scene
+scene.add( light );
 
 var animate = function() {
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
 
     renderer.render(scene, camera);
 };
