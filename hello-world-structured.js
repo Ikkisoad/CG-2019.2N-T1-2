b5e7d6bbce1014912e1a598cfafb5a4b9eb2a49a
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2019-12-10 12:46:15 -0300

    Tutorial

--- hello-world-structured.js
+++ hello-world-structured.js
@@ -1,3 +1,5 @@
+//let controls;
+
 //SCENE
 var scene = new THREE.Scene();
 scene.background = new THREE.Color('#3d3d3d');
@@ -12,26 +14,58 @@ var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.set(0,0,10);
 camera.position.z = 5;
 
+/*function createControls() {
+
+    controls = new THREE.OrbitControls( camera, window );
+  
+  }
+
+createControls();*/
+
 //RENDERER
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//CUBE
-var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-//var material = new THREE.MeshBasicMaterial({color: 0xff0000});
-var material = new THREE.MeshStandardMaterial({color: 0xff0000});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//LIGHT
+/*const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
+scene.add( ambientLight );
+const mainLight = new THREE.DirectionalLight( 0xffffff, 1 );
+mainLight.position.set( 10, 10, 10 );
+scene.add( ambientLight, mainLight );*/
+const ambientLight = new THREE.HemisphereLight(
+    0xddeeff, // bright sky color
+    0x202020, // dim ground color
+    3, // intensity
+);
 
-// Create a directional light
-const light = new THREE.DirectionalLight( 0xffffff, 5.0 );
-// move the light back and up a bit
-light.position.set( 10, 10, 10 );
-// remember to add the light to the scene
-scene.add( light );
+scene.add( ambientLight );
 
-var animate = function() {
+// create a texture loader.
+const textureLoader = new THREE.TextureLoader();
+
+  // Load a texture. See the note in chapter 4 on working locally, or the page
+  // https://threejs.org/docs/#manual/introduction/How-to-run-things-locally
+  // if you run into problems here
+  const texture = textureLoader.load( 'img/uv.png' );
+  // set the "color space" of the texture
+  texture.encoding = THREE.sRGBEncoding;
+  // reduce blurring at glancing angles
+  texture.anisotropy = 16;
+
+  // create a Standard material using the texture we just loaded as a color map
+  const material = new THREE.MeshStandardMaterial( {
+    map: texture,
+  } );
+
+  //CUBE
+    var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+    //var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    //var material = new THREE.MeshStandardMaterial({color: 0xff0000});
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+/*var animate = function() {
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
@@ -39,6 +73,29 @@ var animate = function() {
     cube.rotation.z += 0.01;
 
     renderer.render(scene, camera);
-};
+};*/
 
-animate();
+function update() {
+
+    // increase the mesh's rotation each frame
+    cube.rotation.z += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
+
+ // start the animation loop
+ renderer.setAnimationLoop( () => {
+
+    update();
+    render();
+
+  } );
+
+//animate();
